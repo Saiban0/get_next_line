c18e8cce92c04602bbb3463f88053df0927fa72e
Saiban <60969163+Saiban0@users.noreply.github.com>
2024-01-22 15:08:33 +0100

    Upload gnl

---
+++ get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/21 14:44:07 by bchedru           #+#    #+#             */
+/*   Updated: 2023/11/30 18:43:56 by bchedru          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+int		ft_strlen(char *str);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strcpy(char *dst, char *src);
+int		ft_find_n(char *str);
+char	*ft_calloc(int nmemb, int size);
+char	*ft_read2buff(char *line, char *buffer, int fd);
+
+#endif
